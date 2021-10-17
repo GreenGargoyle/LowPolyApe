56912d419b59fd4d6cef153b5b0a4b66399f8eb0
GreenGargoyle <gkenhur@gmail.com>
2021-10-17 10:01:38 +0100

    Memory subsystem initial implementation.

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -2,8 +2,7 @@
 
 #include <entry.h>
 
-// TODO: Remove this.
-#include <platform/platform.h>
+#include <core/lpamemory.h>
 
 // Defines the function to create a game.
 b8 create_game(game* out_game)
@@ -21,7 +20,7 @@ b8 create_game(game* out_game)
     out_game->on_resize = game_on_resize;
 
     // Create the game state.
-    out_game->state = platform_allocate(sizeof(game_state), FALSE);
+    out_game->state = lpaallocate(sizeof(game_state), MEMORY_TAG_GAME);
 
     return TRUE;
 }
